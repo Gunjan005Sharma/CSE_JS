19284134f3ca8a215ec7a59d2d8b9140ed2b771f
Gunjan005Sharma <gunjanash2006@gmail.com>
2026-02-24 10:35:29 +0530

    code.html

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-var num =10;
-console.log(num);
-console.log(typeof(num));
-num=null;
-console.log(num);
-console.log(typeof(num));
-num=undefined;
-console.log(num);
-console.log(typeof(num));
-//function
-var sayhello=function(){
-    alert("hello world!");
+var count;
+document.write("starting loop"+"<br/>");
+for(count=0;count<10;count++){
+    document.write("current count:"+count+"<br/>");
 }
-console.log(sayhello);
-// symbol type
-const sym1= Symbol(4);
-console.log(sym1);
-const sym2=Symbol(4);
-console.log(sym2);
-if(sym1==sym2){
-    console.log("true");
-}
-else{
-    console.log("false");
-}
+document.write("loop stopped");
