529b7a7a15fb2ba8e3fa0cb1558a0065cee768c4
Gunjan005Sharma <gunjanash2006@gmail.com>
2026-02-24 09:55:11 +0530

    basis of java script

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-console.log("HELLO WORLD");
-document.write("hello world");
+var num =10;
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+//function
+var sayhello=function(){
+    alert("hello world!");
+}
+console.log(sayhello);
+// symbol type
+const sym1= Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
